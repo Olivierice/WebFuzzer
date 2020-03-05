f2c56730dcc1c78ca49d6a9602a32b1c8d6db44e
root <olivier.tuhault@gmail.com>
2020-03-05 16:27:25 -0500

    update

--- perFuzz.py
+++ perFuzz.py
@@ -7,11 +7,17 @@ import urllib.error as E
 import sys, getopt
 
 def usage():
-    print("""
----------------------------------------------------------------------------------------
-| Usage: python3 perFuzz.py -u target_ip -t nb_threads -f word_list.txt -e extensions |
---------------------------------------------------------------------------------------- 
-    """)
+    print("     - Python Fuzzing Tool -")
+    print()
+    print("     Usage: perFuzz.py -u target_ip -f word_list")
+    print()
+    print("     -u --target      -   ip or hostname of the target")
+    print("     -f --wordlist    -   wordlist to use to fuzz ")
+    print("     -t --threads     -   threads number dedicated to fuzz, have to be an integer")
+    print("     -e --extensions  -   extensions you want to add after the words in the word_list")
+    print()
+
+
 
 
 def get_args(argv):
@@ -24,16 +30,24 @@ def get_args(argv):
                 if opt == '-h':
                     usage()
                     sys.exit(2)
+
                 elif opt in ("-u","--url"):
                     target_url = arg
                     print("L'argument en face de -u est: {}".format(target_url))
 
                 elif opt in ("-t","--threads"):
-                    nb_threads = arg
+                    try:
+                        nb_threads = int(arg)
+                    except:
+                        usage()
+                        print("/!\\ Warning /!\\    -t --threads - have to be an integer")
+                        sys.exit(2)
                     print("L'argument en face de -t est: {}".format(nb_threads))
+
                 elif opt in ("-f","--wordlist"):
                     list_file = arg
                     print("L'argument en face de -f est: {}".format(list_file))
+
                 elif opt in ("-e","--extension"):
                     exts = arg
                     print("L'argument en face de -e est: {}".format(exts))
